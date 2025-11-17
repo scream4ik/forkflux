270e382138114f085d3ee9cd95c6f518f3af3355
Slava <slava.deb@gmail.com>
2025-11-17 14:55:14 -0500

    chore: replace gpt-5 by gpt-5.1

--- app/constants.py
+++ app/constants.py
@@ -6,7 +6,7 @@ class LLMModel(str, Enum):
     GPT_4_1_MINI = "gpt-4.1-mini"
     GPT_4_1_NANO = "gpt-4.1-nano"
     GPT_4O_MINI = "gpt-4o-mini"
-    GPT_5 = "gpt-5"
+    GPT_5_1 = "gpt-5.1"
     GPT_5_MINI = "gpt-5-mini"
 
 
